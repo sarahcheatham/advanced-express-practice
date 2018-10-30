e94221c528b9c099d49fd4243e988050b5399e1a
sarahcheatham <sarah.cheatham14@gmail.com>
2018-10-29 20:35:55 -0500

    express server working

--- server/index.js
+++ server/index.js
@@ -4,3 +4,11 @@ let products = require("./products");
 let vehicles = require("./vehicles");
 let contacts = require("./contacts");
 
+const app = express();
+app.use(bodyParser.json());
+
+app.use(express.static('static'));
+
+app.listen(3001, function(){
+    console.log('app started on port 3001')
+});
